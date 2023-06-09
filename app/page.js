e140bfa064d001100242c75b1f2640a3756ebbf4
Abu Taher <writerabutaher@gmail.com>
2023-06-09 20:39:26 +0600

    project setup done

---
+++ app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className='text-9xl'>Float Fashion</h1>
+    </main>
+  )
+}
