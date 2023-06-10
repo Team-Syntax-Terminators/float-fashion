b5b0c0a32bddd33c052262ee2a10e8fe55d3de93
Md. Asif Ullah Sikder <108382626+asifsikder23@users.noreply.github.com>
2023-06-10 11:27:55 +0600

    dfgdfh

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
     <main className="flex min-h-screen items-center justify-center">
       <h1 className='text-9xl'>Float Fashion</h1>
       <h1 className='text-9xl'>Float</h1>
+      <h1 className='text-9xl'>Float</h1>
     </main>
   )
 }
