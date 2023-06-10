f1358e6b11b5562a45a06adbb0b904acca7d7eab
Abu Taher <writerabutaher@gmail.com>
2023-06-10 11:46:58 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className='text-9xl'>Float Fashion</h1>
+      <h1 className='text-9xl'>Float Fashion</h1>
     </main>
   )
 }
