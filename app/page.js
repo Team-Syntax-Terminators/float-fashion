f66eb3811d7f4e4129600163cfe0b1cdf3637bc4
Md. Asif Ullah Sikder <108382626+asifsikder23@users.noreply.github.com>
2023-06-10 11:49:57 +0600

    conflict khan

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="container">
       <h1 className='text-9xl'>Float Fashion</h1>
       <h1 className='text-9xl'>Float Fashion</h1>
     </main>
