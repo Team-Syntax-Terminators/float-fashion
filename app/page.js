6f147389d45efbc3494169cff8a6db34ab902236
Md. Asif Ullah Sikder <108382626+asifsikder23@users.noreply.github.com>
2023-06-10 11:54:39 +0600

    dgfh

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
     <main className="container mx-auto">
       <h1 className='text-9xl'>Float Fashion</h1>
       <h1 className='text-9xl'>Float Fashion</h1>
+      <h1 className='text-9xl'>Float Fashion</h1>
     </main>
   )
 }
