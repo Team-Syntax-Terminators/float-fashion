427c45ed3dd2578ee885901a2648b1f91a928597
Abu Taher <writerabutaher@gmail.com>
2023-06-18 09:02:49 +0600

    footer done

--- app/i18next.js
+++ app/i18next.js
@@ -1,11 +1,11 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
-  
+
   .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
@@ -15,13 +15,12 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'en',
+    fallbackLng: "en",
     debug: true,
 
     interpolation: {
       escapeValue: false,
-    }
+    },
   });
-
 
 export default i18n;
