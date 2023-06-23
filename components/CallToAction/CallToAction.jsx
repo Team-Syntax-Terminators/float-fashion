ffa3d93f28d409d0ea420b182b5647315f4667b1
Abu Taher <writerabutaher@gmail.com>
2023-06-23 22:34:20 +0600

    footer modify

--- components/CallToAction/CallToAction.jsx
+++ components/CallToAction/CallToAction.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import "../../styles/fashion/_cta.scss";
 
 const CallToAction = () => {
   return (
-    <div>CallToAction</div>
-  )
-}
+    <div className="container mx-auto">
+      <div id="container">
+        <div id="content">
+          <h1>
+            Buy now and benefit up to <span>30% off</span>
+          </h1>
+          <p>
+            Be part of millions people around the world using Float Fashion in
+            modern clothing.
+          </p>
+          <button>Shop Now</button>
+          <button>More About</button>
+        </div>
+        <div id="images">
+          <img
+            src=""
+            alt=""
+          />
+          <img
+            src=""
+            alt=""
+          />
+          <img
+            src=""
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CallToAction
+export default CallToAction;
