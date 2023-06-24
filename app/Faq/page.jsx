e7134bb481ec6e713ff7ede8daaa12b876c9e5f7
Md. Asif Ullah Sikder <108382626+asifsikder23@users.noreply.github.com>
2023-06-24 22:26:10 +0600

    faq

--- app/Faq/page.jsx
+++ app/Faq/page.jsx
@@ -1,6 +1,9 @@
+"use client"
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import '../../styles/fashion/_faq.scss'
 import Head from 'next/head';
-import React from 'react';
+import React, { useState } from 'react';
+import AddIcon from "@mui/icons-material/Add";
 
 const Faq = () => {
     return (
@@ -9,6 +12,7 @@ const Faq = () => {
                 <title>FAQ | Float Fashion</title>
             </Head>
             <Hero />
+            <Students />
         </>
     );
 };
@@ -22,10 +26,18 @@ const Hero = () => {
             <div className="mx-auto max-w-screen-xl pt-5">
                 <div className="md:flex justify-center gap-5 items-center">
                     <div className='md:w-1/2'>
-                        <h1 className='text-xl font-bold'>Frequently Asked Questions</h1>
-                        <p>Float Fashion understands the importance of keeping the FAQ section up to date. They regularly review and update the information to ensure its accuracy and relevance. As new products are introduced, policies are revised, or common customer queries arise, the FAQ section is promptly updated to reflect the latest information.</p>
+                        <h1 className='text-2xl font-bold py-2' style={{
+                            fontFamily: 'sans-serif'
+                        }}>
+                            Frequently Asked Questions
+                        </h1>
+                        <p style={{
+                            fontFamily: 'sans-serif'
+                        }}>
+                            Float Fashion understands the importance of keeping the FAQ section up to date. They regularly review and update the information to ensure its accuracy and relevance. As new products are introduced, policies are revised, or common customer queries arise, the FAQ section is promptly updated to reflect the latest information.
+                        </p>
                     </div>
-                    <div>
+                    <div className='flex justify-center'>
                         <img src="/assets/faq/faq.jpg" alt="" />
                     </div>
                 </div>
@@ -66,3 +78,72 @@ const Hero = () => {
         </section>
     )
 }
+const Students = () => {
+    const [expanded, setExpanded] = useState(false);
+    const faqlist = [
+        {
+            question: 'What is Fashion Float?',
+            answer: 'Fashion Float is an online clothing store that offers a wide range of trendy fashion items for men, women and kids.',
+        },
+        {
+            question: 'How can I place an order?',
+            answer: 'To place an order, simply browse our website, select the desired items, add them to your shopping cart, and proceed to the checkout page.',
+        },
+        {
+            question: 'What payment methods do you accept?',
+            answer: 'We accept major credit cards, PayPal, and bank transfers as payment methods.',
+        },
+        {
+            question: 'How long does shipping take?',
+            answer: 'Shipping times may vary depending on your location. Typically, orders are delivered within 5-7 business days for domestic shipments and 7-14 business days for international shipments.',
+        },
+        {
+            question: 'Can I return or exchange items?',
+            answer: 'Yes, we have a hassle-free return and exchange policy. If you\'re not satisfied with your purchase, you can return or exchange it within 30 days of receiving the order. Please refer to our Returns and Exchanges page for more details.',
+        },
+        {
+            question: 'Does Float Fashion offer international shipping?',
+            answer: 'Yes, Float Fashion provides international shipping to customers around the world. During the checkout process, you will be prompted to enter your shipping address, and available shipping options and associated fees will be displayed based on your location.',
+        },
+        {
+            question: 'Does Float Fashion offer any promotions, discounts, or rewards?',
+            answer: 'Float Fashion may offer promotions, discounts, or rewards programs from time to time. Check their website or subscribe to their newsletter to stay updated on any ongoing promotions, exclusive discounts, or loyalty programs they may offer.',
+        },
+    ]
+    const handleChange = (isExpanded, i) => {
+        setExpanded(isExpanded ? i : false);
+    };
+    return (
+        <div className="mx-auto max-w-screen-xl my-5 paddingBottom" id='accord'>
+            <div className='md:grid grid-cols-2 items-center gap-5'>
+                <div>
+                    <img src="https://cdn.dribbble.com/users/985415/screenshots/6290809/clothing.gif" />
+                </div>
+                <div className='customfaqlist'>
+                    <h1 className='text-center text-2xl font-bold'>FAQ</h1>
+                    {faqlist.map((q, i) => (
+                        <Accordion
+                            elevation={0}
+                            key={i}
+                            expanded={expanded === i}
+                            onChange={(event, isExpanded) => handleChange(isExpanded, i)}
+                        >
+                            <AccordionSummary
+                                focusVisibleClassName="text-blue-300"
+                                expandIcon={<AddIcon />}
+                                aria-controls="panel1a-content"
+                            >
+                                <Typography variant="h6" className="font-bold text-lg text-black">
+                                    {q.question}
+                                </Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>{q.answer}</Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
