678c5cad532da5ad8a0ad0872a6f102cabf48172
Md. Asif Ullah Sikder <108382626+asifsikder23@users.noreply.github.com>
2023-06-24 15:53:06 +0600

    privacy

--- app/privacy/page.jsx
+++ app/privacy/page.jsx
@@ -8,11 +8,11 @@ const Privacy = () => {
             <Head>
                 <title>Privacy & Policy | Float Fashion</title>
             </Head>
-            <Hero/>
+            <Hero />
+            <Details />
         </>
     );
 };
-
 export default Privacy;
 const Hero = () => {
     return (
@@ -22,8 +22,65 @@ const Hero = () => {
                 position: 'relative'
             }}>
                 <h1> Privacy & Policy <span className='text-orange-600'>🔑 </span> </h1>
-                <p className='md:w-2/5'>Terms of service are the legal agreements between a service provider and a person who wants to use that service. The person must agree to abide by the terms of service in order to use the offered service.</p>
+                <p className='md:w-2/5'>This Privacy Policy is meant to help you understand what information we collect, why we collect it, and how you can update, manage, export, and delete your information.</p>
             </div>
         </section>
     );
 };
+const Details = () => {
+    const details = [
+      {
+        _id: 1,
+        title: 'Information Collection',
+        description: [
+          {
+            list1: 'Personal Information: We may collect personal information such as your name, email address, shipping address, and payment details when you create an account, place an order, or subscribe to our newsletter. This information is necessary to fulfill your orders and provide a personalized shopping experience.',
+          },
+          {
+            list2: 'Non-Personal Information: We may collect non-personal information such as your IP address, browser type, operating system, and website usage data. This information is collected to improve our website performance, enhance user experience, and analyze trends.',
+          },
+        ],
+      },
+    ];
+  
+    return (
+      <section className="bg-gray-700" id="privacyDetails">
+        <div className="mx-auto max-w-screen-xl">
+          <div className="flex justify-center py-10">
+            <h1 className="text-center w-11/12 text-white" style={{ fontFamily: 'revert' }}>
+              At Fashion Float, we are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy outlines how we collect, use, and disclose the information you provide to us when using our website. By using Fashion Float, you consent to the practices described in this policy.
+            </h1>
+          </div>
+          <div id="cards">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+              {details.map((info) => {
+                const { _id, title, description } = info;
+                return (
+                  <div key={_id} className="p-10 card">
+                    <h1 className="font-bold text-xl text-center pb-5">
+                      {title}
+                    </h1>
+                    <ul>
+                      {description.map((item, index) => {
+                        const listItem = Object.values(item)[0];
+                        return (
+                          <li key={index} className="list-disc" style={{ listStyleType: 'disc' }}>
+                            {listItem}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  
+  
+  
+  
